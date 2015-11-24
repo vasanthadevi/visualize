d7a8c8f311713a85ef095357d5d259b917dc2192
deiveehan <deiveehan@gmail.com>
2015-11-24 13:06:38 -0600

    initial version

---
+++ visualize/src/main/java/visualize/HomeController.java
@@ -0,0 +1,21 @@
+package visualize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HomeController {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hi This is Deiveehan here !!!";
+	}
+
+	public static void main(String args[]) {
+		SpringApplication.run(HomeController.class, args);
+	}
+
+}
